7b7df41b65358e33bd2b5e0c57ae4f899d08301a
sim-max <101747552+sim-max@users.noreply.github.com>
2022-07-30 17:35:55 +0300

    added product section + adaptive this section

--- js/script.js
+++ js/script.js
@@ -1,25 +1,73 @@
-$('.first-slider').slick({
-	dots: true,
-	infinite: true,
-	fade: true,
-	prevArrow: '<button type="button" class="slick-prev"><img src="img/first/arrow-prev.svg" alt="Previos"></button>',
-	nextArrow: '<button type="button" class="slick-next"><img src="img/first/arrow-next.svg" alt="Next"></button>',
-	responsive: [
-		{
-			breakpoint: 769,
-			settings: {
-				prevArrow: '<button type="button" class="slick-prev"><img src="img/first/arrow-prev-grey.svg" alt="Previos"></button>',
-				nextArrow: '<button type="button" class="slick-next"><img src="img/first/arrow-next-grey.svg" alt="Next"></button>',
-			}
-		},
-	],
-});
+$(function () {
+	$('.first-slider').slick({
+		dots: true,
+		infinite: true,
+		fade: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/first/arrow-prev.svg" alt="Previos"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/first/arrow-next.svg" alt="Next"></button>',
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					prevArrow: '<button type="button" class="slick-prev"><img src="img/first/arrow-prev-grey.svg" alt="Previos"></button>',
+					nextArrow: '<button type="button" class="slick-next"><img src="img/first/arrow-next-grey.svg" alt="Next"></button>',
+				}
+			},
+		],
+	});
+
+	/*--- Product sliders ----*/
+	$('.product-slider-info').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		vertical: true,
+		verticalSwiping: true,
+		asNavFor: '.product-slider-title',
+		responsive: [
+			{
+				breakpoint: 1001,
+				settings: {
+					vertical: false,
+				}
+			},
+		],
+
+	});
+	$('.product-slider-title').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.product-slider-info',
+		arrows: true,
+		focusOnSelect: true,
+		centerMode: true,
+		vertical: true,
+		prevArrow: '<button type="button" class="product-slider-title__prev slick-prev"><img src="img/product/prev.svg" alt="Previos"></button>',
+		nextArrow: '<button type="button" class="product-slider-title__next slick-next"><img src="img/product/next.svg" alt="Next"></button>',
+		responsive: [
+			{
+				breakpoint: 1001,
+				settings: {
+					vertical: false,
+					slidesToShow: 3,
+				},
+				breakpoint: 769,
+				settings: {
+					slidesToShow: 1,
+					vertical: false,
+				}
+			},
+		],
+	});
+
+})
+
 
 /*---Menu---*/
 const menuBtn = document.querySelector('.burger-btn');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
-const menuLinks = document.querySelectorAll('.menu__link')
+const menuLinks = document.querySelectorAll('.menu a')
 
 menuBtn.addEventListener('click', function () {
 	body.classList.toggle('no-scroll');
@@ -29,8 +77,8 @@ menuBtn.addEventListener('click', function () {
 })
 menuLinks.forEach(function (link) {
 	link.addEventListener('click', function () {
-		menuList.classList.remove('active');
+		menu.classList.remove('active');
 		body.classList.remove('no-scroll');
-		menuBtn.classList.remove('active-btn');
+		menuBtn.classList.remove('active');
 	})
 })
